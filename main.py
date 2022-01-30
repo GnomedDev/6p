418a5573d8f49d2e5c59e2ef248d1e54231f2477
aleclol <50505980+aleclol@users.noreply.github.com>
2022-01-30 17:44:22 -0500

    styl

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from fastapi import FastAPI, Response, Request
 import ipaddress
-import aiohttp
-import random
-from dotenv import load_dotenv
 import os
+import random
+
+import aiohttp
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, Response
 
 load_dotenv()
 
 IPV6_BLOCK = os.getenv("IPV6_BLOCK")
 
+
 def get_random_ipv6():
     network = ipaddress.IPv6Network(IPV6_BLOCK)
-    size = int(network[-1]) - int(network[0]) + 1 
+    size = int(network[-1]) - int(network[0]) + 1
     random_index = random.randint(0, size)
     return str(network[random_index])
 
+
 app = FastAPI()
+
 
 @app.get("/{url:path}")
 async def root(request: Request, url: str):
@@ -28,8 +32,12 @@ async def root(request: Request, url: str):
 
     connector = aiohttp.TCPConnector(local_addr=(get_random_ipv6(), 0))
     try:
-        async with aiohttp.ClientSession(headers=headers, connector=connector) as session:
+        async with aiohttp.ClientSession(
+            headers=headers, connector=connector
+        ) as session:
             async with session.get(url) as response:
-                return Response(content=await response.read(), status_code=response.status)
+                return Response(
+                    content=await response.read(), status_code=response.status
+                )
     except aiohttp.client_exceptions.InvalidURL:
         return Response(status_code=404)
